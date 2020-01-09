ca9ce122f3ad94236610fe0e83de41f419361222
Thomas Foster <thomasvfoster@gmail.com>
2020-01-09 21:53:07 +0000

    set up the simulation canvas

--- main.js
+++ main.js
@@ -18,9 +18,37 @@ function paint_c_space(ctx) {
     }
 }
 
+function paint_robot(ctx, robot_state) {
+
+    ctx.translate(240, 180);
+    ctx.scale(-100, 100);
+    ctx.rotate(Math.PI / 2);
+
+    var draw_radius = 0.05;
+
+    ctx.beginPath();
+    ctx.arc(-0.5, 0, draw_radius, 0, 2 * Math.PI, true);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(0.5, 0, draw_radius, 0, 2 * Math.PI, true);
+    ctx.fill();
+}
+
 function main() {
+
+    var robot_state = {
+        alpha: Math.pi / 2,
+        alpha_prime: Math.pi / 2
+    };
+
     var c_space_canvas = document.getElementById('c-space');
     var c_space_ctx = c_space_canvas.getContext('2d');
 
     paint_c_space(c_space_ctx);
+
+    var simulation_canvas = document.getElementById('simulation');
+    var simulation_ctx = simulation_canvas.getContext('2d');
+
+    paint_robot(simulation_ctx, robot_state);
 }
